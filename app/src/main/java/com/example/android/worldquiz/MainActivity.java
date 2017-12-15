d955855ad2fd60e093be3acaa52e709e5ebf4237
frac-tal <12370607+frac-tal@users.noreply.github.com>
2017-12-15 17:15:51 +0100

    started to work on question layout, button enables when text entered

--- app/src/main/java/com/example/android/worldquiz/MainActivity.java
+++ app/src/main/java/com/example/android/worldquiz/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.worldquiz;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mSectionsStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
         mViewPager = (ViewPager) findViewById(R.id.container);
         setupViewPager(mViewPager);
--- app/src/main/java/com/example/android/worldquiz/QuestionFragment.java
+++ app/src/main/java/com/example/android/worldquiz/QuestionFragment.java
@@ -3,11 +3,14 @@ package com.example.android.worldquiz;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -20,21 +23,22 @@ public class QuestionFragment extends Fragment {
     private static final String TAG = "QuestionFragment";
 
     private static final String TITLE = "page_title";
-    private static final String MESSAGE = "page_message";
+    private static final String CONTENT = "page_message";
 
     String title;
-    String message;
+    String content;
 
     Button nextButton;
-    TextView question_title;
-    TextView question_content;
+    TextView questionTitle;
+    TextView questionContent;
+    EditText userAnswer;
 
-    public static QuestionFragment newInstance(String title, String message)
+    public static QuestionFragment newInstance(String question_title_text, String question_content_text)
     {
         QuestionFragment f = new QuestionFragment();
         Bundle bdl = new Bundle(2);
-        bdl.putString(TITLE, title);
-        bdl.putString(MESSAGE, message);
+        bdl.putString(TITLE, question_title_text);
+        bdl.putString(CONTENT, question_content_text);
         f.setArguments(bdl);
         return f;
     }
@@ -45,14 +49,30 @@ public class QuestionFragment extends Fragment {
     public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container, @Nullable final Bundle savedInstanceState) {
         Bundle arguments = getArguments();
         title = arguments.getString(TITLE);
-        message = arguments.getString(MESSAGE);
+        content = arguments.getString(CONTENT);
 
         View view = inflater.inflate(R.layout.question_layout, container, false);
-        question_title = (TextView) view.findViewById(R.id.question_title);
-        question_content = (TextView) view.findViewById(R.id.question_content);
+        questionTitle= (TextView) view.findViewById(R.id.question_title);
+        questionContent = (TextView) view.findViewById(R.id.question_content);
+        questionTitle.setText(title);
+        questionContent.setText(content);
+        
+        userAnswer = (EditText) view.findViewById(R.id.answer_edit_text);
+        userAnswer.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
-        question_title.setText(title);
-        question_content.setText(message);
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                enableButtonIfText();
+            }
+        });
 
         nextButton = (Button) view.findViewById(R.id.next);
         nextButton.setOnClickListener(new View.OnClickListener() {
@@ -71,5 +91,12 @@ public class QuestionFragment extends Fragment {
     public void nextFragment(View view){
         int currentFragmentNumber = ((MainActivity)getActivity()).getViewPager();
         ((MainActivity)getActivity()).setViewPager(currentFragmentNumber + 1);
+    }
+
+    /**
+     * Enable the button if an answer was given, else disable it.
+     */
+    public void enableButtonIfText(){
+        nextButton.setEnabled(userAnswer.getText().toString().length()>0);
     }
 }
